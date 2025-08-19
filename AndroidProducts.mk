44e05e125831099fd67314bdb90bc4091223d690
khusnan21 <khusnan21@gmail.com>
2025-08-19 22:36:40 +0700

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_P1102GT.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_P1102GT-eng
